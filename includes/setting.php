86480dc6b95a9798ebcd743364ff98b74b833dce
mohit <mohit@mohit>
2015-06-03 11:58:08 +0530

    Test profile info

--- includes/setting.php
+++ includes/setting.php
@@ -1,5 +1,5 @@
 <?php
-	define("server",'laptop');//server = laptop,server,poorvi
+	define("server",'poorvi');//server = laptop,server,poorvi
 
 	if(server=="poorvi"){
 		define('HOST','http://poorvi.cse.iitd.ac.in/~cs1120233/teaching/',false); // false mean that HOST cannot be used as hOsT
@@ -40,16 +40,6 @@
 		$db_data['user'] = 'root' ;
 		$db_data['pass'] = 'mohit' ;
 		$db_data['db'] = 'teaching' ;
-	}
-	else if(server=="freeserver"){
-		define('HOST','http://knowmiles.hostingsiteforfree.com/knowmiles/',false); // false mean that HOST cannot be used as hOsT
-		define('ROOT', '/home/u274166964/public_html/knowmiles/', false);
-		define('CDN', HOST.'photo/', false);
-		$db_data = array() ;
-		$db_data['host'] = 'mysql.1freehosting.com' ;
-		$db_data['user'] = 'u274166964_gsk' ;
-		$db_data['pass'] = 'KnowMiles696' ;
-		$db_data['db'] = 'u274166964_test1' ;
 	}
 
 	define('BASE',HOST."index.php/welcome/",false); // false mean that HOST cannot be used as hOsT
