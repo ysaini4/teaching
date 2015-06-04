72ec1423dfa6ff0bd8478f67c2e7cd89f4306979
Mohit <mohitsaini1196@gmail.com>
2015-06-04 14:35:03 +0530

    Changed pull

--- pull.php
+++ pull.php
@@ -2,8 +2,12 @@
 include "includes/app.php";
 
 if($_GET["key"]=="mohit112"){
-	echo shell_exec("git pull");
+	echo shell_exec("git pull;chmod 777 * -R");
+	echo "Done ! ";
 }
+
+
+
 
 closedb();
 ?>
