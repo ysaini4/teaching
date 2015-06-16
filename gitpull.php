2f1822e59697fc879cbe826adb76dea3d2dc2d66
Mohit <bhavani.cse.iitd.ac.in>
2015-06-16 20:54:57 +0530

    11

--- gitpull.php
+++ gitpull.php
@@ -1,11 +1,18 @@
 <?php
 include "includes/app.php";
 
-if(get("key")="vm341sm"){
+
+
+if(get("key")=="vm341sm"){
 	$cmd="git pull -u origin master";
 	echo shell_exec($cmd);
 	echo shell_exec("chmod 777 * -R");
 }
+else{
+	echo "Key not matched";
+}
+ 
+
 
 closedb();
 ?>
