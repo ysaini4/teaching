888911a4f2057f0117075e911709090ef2d452aa
mohit <mohit@mohit>
2015-06-16 10:53:59 +0530

    Linking

--- application/views/Template/navbarnew.php
+++ application/views/Template/navbarnew.php
@@ -2,7 +2,9 @@
   <!-- Account Dropdown -->
   <ul id="dropdownaccount" class="dropdown-content">
     <li><a href="<?php echo BASE."login" ;?>">Login</a></li>
-    <li><a href="<?php echo BASE."signup" ;?>">Signup</a></li>
+
+<!--     <li><a href="<?php echo BASE."signup" ;?>">Signup</a></li>
+ -->
     <li><a href="<?php echo BASE."joinus" ;?>">Join Us</a></li>
 
   </ul>
@@ -13,8 +15,8 @@
         <a href="#" class="brand-logo">getIITians</a>
         <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><a href="<?php echo BASE."about" ;?>">About</a></li>
-          <li><a href="<?php echo BASE."contact" ;?>">Contact us</a></li>
+          <li><a href="<?php echo BASE."aboutus" ;?>">About</a></li>
+          <li><a href="<?php echo BASE."contactus" ;?>">Contact us</a></li>
           <li><a class="dropdown-button" href="#" data-beloworigin="true" data-activates="dropdownaccount"><i class="mdi-action-account-circle left"></i></a></li>
           <form class="right">
             <div class="input-field teal lighten-1">
@@ -29,10 +31,13 @@
   </div>
   <!-- SideNav -->
   <ul class="side-nav" id="mobile-demo">
-    <li><a href="#">About</a></li>
-    <li><a href="#">Contact</a></li>
-    <li><a href="<?php echo BASE."login" ;?>">Login</a></li>
-    <li><a href="<?php echo BASE."signup" ;?>">Signup</a></li>
+    <li><a href="<?php echo BASE."aboutus" ;?>" >About</a></li>
+    <li><a href="<?php echo BASE."contactus" ;?>" >Contact</a></li>
+    <li><a href="<?php echo BASE."login" ;?>" >Login</a></li>
+    <li><a href="<?php echo BASE."joinus" ;?>" >Join us</a></li>
+
+<!--     <li><a href="<?php echo BASE."signup" ;?>">Signup</a></li>
+ -->
     <li>
       <form>
         <div class="input-field">
