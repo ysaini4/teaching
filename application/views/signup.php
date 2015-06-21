33310a8aabc16bc183722f34391bdf2c4009e865
Shivam Mamgain <shivammamgain47@gmail.com>
2015-06-21 18:39:11 +0530

    Updated signup page

--- application/views/signup.php
+++ application/views/signup.php
@@ -8,91 +8,101 @@ $defopen="signupwindow";
 ?>
   <main>
     <div class="container">
-      <div class="container">
-      <div class="card-panel">
-        <div class="row">
-          <div align='center' style='color:red;' ><p><?php echo $signupmsg; ?></p></div>
-          <div class="col s12">
-            <h4 class="teal-text text-darken-1 center" style="font-weight:bold; font-variant: small-caps;">Sign Up</h4>
+    <br>
+      <div class="row">
+        <div class="col s12 l10 offset-l1">
+          <div class="card-panel">
+            <?php
+            if (!empty($signupmsg)) {
+                echo '<div class="row"><div class="col s12 l10 offset-l1">';
+                echo '<div class="card-panel red white-text center">' . $signupmsg . '</div>';
+                echo '</div></div>';
+            }
+            ?>
+            <div class="row">
+              <div class="col s12">
+                <h4 class="teal-text text-darken-1 center">Sign Up</h4>
+              </div>
+            </div>
+            <div class="row center">
+              <form class="col s12 l10 offset-l1" method="post" onsubmit='return ms.signupform(this,<?php echo tf($_ginfo["needsignupotp"]); ?>);' <?php if($_ginfo["needsignupotp"]) { ?>  data-action='signupotp' data-params='{"phone":$("#signupwindow").find("input[name=phone]").val()}' data-res='hideshowdown("signupwindow","otpwindow");'  <?php }else{ ?>  <?php } ?>  >
+                <div id="signupwindow" style='<?php dit($defopen=="signupwindow"); ?>' >
+                  <div class="row">
+                    <div class="input-field col s12 l6">
+                      <input id="fullname" name="name" type="text" data-condition="simple" class="validate" required>
+                      <label for="fullname">Full Name</label>
+                    </div>
+                    <div class="input-field col s12 l6">
+                      <input id="email" name="email" type="email" data-condition="email" class="validate" required>
+                      <label for="email">Email</label>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="input-field col s12 l6">
+                      <input id="password" name="password" type="password"  data-condition="simple" class="validate" required>
+                      <label for="password">Password</label>
+                    </div>
+                    <div class="input-field col s12 l6">
+                      <input id="confirm_password" name="cpassword" type="password" data-condition="password" class="validate" required>
+                      <label for="confirm_password">Confirm Password</label>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="input-field col s12">
+                      <input id="phone" name="phone" type="text" data-condition="simple" class="validate" required>
+                      <label for="phone">Mobile Number</label>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="input-field col s12">
+                      <button class="btn waves-effect waves-light" name="signup" type="submit" id="submit_button">Submit</button>
+                      <button class="btn grey waves-effect waves-light" type="reset">Reset</button>
+                    </div>
+                  </div>
+                </div>
+                <div id="otpwindow" style='<?php dit($defopen=="otpwindow"); ?>' >
+
+                  <div class="col s12">
+                    Please confirm your number
+                  </div>
+                  <div class="row">
+                    <div class="input-field col s12">
+                      <input id="otp" name="otp" type="text" data-condition="simple" class="validate" >
+                      <label for="otp">One Time Password</label>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="input-field col s12">
+                      <button class="btn waves-effect waves-light"  name="signup" type="submit" >Submit
+                        <i class="mdi-content-send right"></i>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-        <div class="row center">
-          <form class="col s12 m10 offset-m1" method="post" onsubmit='return ms.signupform(this,<?php echo tf($_ginfo["needsignupotp"]); ?>);' <?php if($_ginfo["needsignupotp"]) { ?>  data-action='signupotp' data-params='{"phone":$("#signupwindow").find("input[name=phone]").val()}' data-res='hideshowdown("signupwindow","otpwindow");'  <?php }else{ ?>  <?php } ?>  >
-            <div id="signupwindow" style='<?php dit($defopen=="signupwindow"); ?>' >
-              <div class="row">
-                <div class="input-field col s12 m12">
-                  <input id="fullname" name="name" type="text" data-condition="simple" class="validate">
-                  <label for="fullname">Full Name</label>
-                </div>
-              </div>
-              <div class="row">
-                <div class="input-field col s12 m12">
-                  <input id="email" name="email" type="email" data-condition="email" class="validate">
-                  <label for="email">Email</label>
-                </div>
-              </div>
-              <div class="row">
-                <div class="input-field col s12 m12">
-                  <input id="password" name="password" type="password"  data-condition="simple" class="validate">
-                  <label for="password">Password</label>
-                </div>
-              </div>
-
-
-              <div class="row">
-                <div class="input-field col s12 m12">
-                  <input id="confirm_password" name="cpassword" type="password" data-condition="password" class="validate">
-                  <label for="confirm_password">Confirm Password</label>
-                </div>
-              </div>
-
-              <div class="row">
-                <div class="input-field col s12 m12">
-                  <input id="phone" name="phone" type="text" data-condition="simple" class="validate">
-                  <label for="phone">Phone</label>
-                </div>
-              </div>
-
-
-              <div class="row">
-                <div class="input-field col s12 m12">
-                  <button class="btn waves-effect waves-light" name="signup" type="submit" >Submit
-                    <i class="mdi-content-send right"></i>
-                  </button>
-                  <button class="btn waves-effect waves-light" type="reset">Reset</button>
-                </div>
-              </div>
-            </div>
-            <div id="otpwindow" style='<?php dit($defopen=="otpwindow"); ?>' >
-
-              <div class="col s12 m12">
-                Please confirm your number
-              </div>
-              <div class="row">
-                <div class="input-field col s12 m12">
-                  <input id="otp" name="otp" type="text" data-condition="simple" class="validate" >
-                  <label for="otp">One Time Password</label>
-                </div>
-              </div>
-              <div class="row">
-                <div class="input-field col s12 m12">
-                  <button class="btn waves-effect waves-light"  name="signup" type="submit" >Submit
-                    <i class="mdi-content-send right"></i>
-                  </button>
-                </div>
-              </div>
-
-
-
-
-
-            </div>
-          </form>
-        </div>
       </div>
-    </div>
+    <br><br>
     </div>
   </main>
+
+<script type="text/javascript">
+  var password = document.getElementById("password");
+  var confirm_password = document.getElementById("confirm_password");
+
+  function validatePassword() {
+    if (password.value != confirm_password.value) {
+      confirm_password.setCustomValidity("Passwords do not match");
+    }
+    else {
+      confirm_password.setCustomValidity("");
+    }
+  }
+
+  document.getElementById('submit_button').onclick = validatePassword;
+</script>
 <?php
 load_view("Template/footer.php",$inp);
 load_view("Template/bottom.php",$inp);
