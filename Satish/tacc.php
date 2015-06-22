025be965710ce9c69e9ad8bf5b203844cdf0ccc6
Shivam Mamgain <shivammamgain47@gmail.com>
2015-06-22 17:06:53 +0530

    Teacher profile - Satish Rana

---
+++ Satish/tacc.php
@@ -0,0 +1,70 @@
+<?php
+
+?>
+ <main>
+  <div class="container_profile"> 
+    <div class="section">
+      <br>
+   <div class="card-panel">
+     <table class="responsive-table "  >
+       <tbody>
+         <tr>
+           <th>TOTAL</th>
+           <th>Fees/hr</th>
+           <td>2000</td> 
+           <td>2000</td> 
+           <td>2000</td> 
+           <td>2000</td> 
+         </tr>
+         <tr> 
+           <td><div class="divider small"></div></td>
+           <th>Hrs</th>
+           <td>14</td>
+           <td>11</td> 
+           <td>20</td> 
+           <td>30</td>
+
+         </tr>
+         <tr>
+            <th>65222</th>
+         	<th>Fees</th>
+         	<td>16</td>
+         	<td>16</td>
+         	<td>16</td>
+         	<td>16</td>
+         </tr>
+         <tr>
+            <td></td>
+            <th></th>
+            <th>Student1</th>
+            <th>Student2</th>
+            <th>Student3</th>
+            <th>Student4</th>
+         </tr>
+         <tr>
+            <th>Daily Earnings</th>
+            <th>Date</th>
+         </tr>
+         <tr>
+            <td>1500</td>
+            <td>1/6/15</td>
+            <td>0</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>   
+         </tr>
+          <tr>
+            <td>1000</td>
+            <td>2/6/15</td>
+            <td>0</td>
+            <td>1</td>
+            <td>1</td>
+            <td>1</td>   
+         </tr>
+      
+      </table>
+    </div> 
+  </div>
+</div>
+<?php
+?>  
