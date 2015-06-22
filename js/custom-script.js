a8f3ac00628650a6f69aa1f5a979b58b20725f9f
Shivam Mamgain <shivammamgain47@gmail.com>
2015-06-22 12:55:27 +0530

    Minor change in custom-script

--- js/custom-script.js
+++ js/custom-script.js
@@ -27,17 +27,4 @@ $(document).ready(function(){
     caption: true
   });
 
-// password and confirm_password validation
-  var password = document.getElementById("password");
-  var confirm_password = document.getElementById("confirm_password");
-  function validatePassword() {
-    if (password.value != confirm_password.value) {
-      confirm_password.setCustomValidity("Passwords do not match");
-    }
-    else {
-      confirm_password.setCustomValidity("");
-    }
-  }
-  document.getElementById('submit_button').onclick = validatePassword;
-
 });
