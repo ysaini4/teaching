a5639ed7e941cb2f1f1cc80010b28d28ea5aaedb
Shivam Mamgain <shivammamgain47@gmail.com>
2015-06-24 17:28:43 +0530

    Home page - Satish

--- application/views/Template/navbarhome.php
+++ application/views/Template/navbarhome.php
@@ -19,16 +19,16 @@
   </ul>
   <!-- NavBar -->
   <div class="navbar-fixed">
-    <nav class="teal" role="navigation">
+    <nav class="transparent" id="navbar" role="navigation">
       <div class="nav-wrapper">
-        <a href="#" class="brand-logo">getIITians</a>
+        <a href="#" class="brand-logo"><img src="images/logo.png" width="190" height="81" style="padding-top:10px;"/></a>
         <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
           <li><a href="<?php echo BASE."aboutus" ;?>">About</a></li>
           <li><a href="<?php echo BASE."contactus" ;?>">Contact us</a></li>
           <li><a class="dropdown-button" href="#" data-beloworigin="true" data-activates="dropdownaccount"><i class="mdi-action-account-circle left"></i>Account</a></li>
           <form class="right">
-            <div class="input-field teal lighten-1">
+            <div class="input-field transparent lighten-1">
               <input id="search" type="search" placeholder="Search" required>
               <label for="search"><i class="mdi-action-search"></i></label>
               <i class="mdi-navigation-close"></i>
