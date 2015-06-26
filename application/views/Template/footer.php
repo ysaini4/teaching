f864275a7893c117e390bbb3841a656e6c67bdb9
Shivam Mamgain <shivammamgain47@gmail.com>
2015-06-26 16:01:49 +0530

    Minor change in footer

--- application/views/Template/footer.php
+++ application/views/Template/footer.php
@@ -1,52 +1,49 @@
-  <!-- Footer -->
-  <footer class="page-footer teal">
-    <div class="container ">
-      <div class="row">
-        <div class="col l4 s12">
-          <h5 class="white-text">Get IITians</h5>
-          <p class="grey-text text-lighten-4"></p>
-          <ul>
-            <li><a class="grey-text text-lighten-3" href="#!">About us</a></li>
-            <li><a class="grey-text text-lighten-3" href="#!">How we work</a></li>
-            <li><a class="grey-text text-lighten-3" href="#!">Our Team</a></li>
-            <li><a class="grey-text text-lighten-3" href=<?php echo BASE."joinus" ;?>>Work with us</a></li>
-          </ul>
-        </div>
-        <div class="col l2 s12">
-          <h5 class="white-text">Help</h5>
-          <ul>
-            <li><a class="grey-text text-lighten-3" href="#!">Refund and Returns</a></li>
-            <li><a class="grey-text text-lighten-3" href="#!">FAQ</a></li>
-            <li><a class="grey-text text-lighten-3" href="#!">Contact us</a></li>
-          </ul>
-        </div>
-        <div class="col l2 s12">
-          <h5 class="white-text">Legal</h5>
-          <ul>
-            <li><a class="grey-text text-lighten-3" href="#!">Privacy Policy</a></li>
-            <li><a class="grey-text text-lighten-3" href="#!">Copyright Policy</a></li>
-            <li><a class="grey-text text-lighten-3" href="#!">Terms of use</a></li>
-          </ul>
-        </div>
-        <div class="col l4 s12">
-          <h5 class="white-text">Contacts</h5>
-          <ul>
-            <li>
-              <a class="grey-text text-lighten-3" href="#!"><img src="images/facebook.png" alt="Facebook" width="30" height="30"/></a>&nbsp;
-              <a class="grey-text text-lighten-3" href="#!"><img src="images/twitter.png" alt="Twitter" width="30" height="30"/></a>&nbsp;
-              <a class="grey-text text-lighten-3" href="#!"><img src="images/google-plus.png" alt="Google+" width="30" height="30"/></a>&nbsp;
-            </li>
-            <li><a class="grey-text text-lighten-3" href="#"><i class="mdi-maps-local-phone"></i> 9876543210</a></li>
-          </ul>
-        </div>
+<!-- Footer -->
+<footer class="page-footer teal">
+  <div class="container ">
+    <div class="row">
+      <div class="col l4 s12">
+        <h5 class="white-text">Get IITians</h5>
+        <p class="white-text"></p>
+        <ul>
+          <li><a class="footer-link" href="<?php echo BASE.'aboutus'; ?>">About Us</a></li>
+          <li><a class="footer-link" href="#!">Our Team</a></li>
+          <li><a class="footer-link" href="#!">Work with us</a></li>
+        </ul>
+      </div>
+      <div class="col l2 s12">
+        <h5 class="white-text">Help</h5>
+        <ul>
+          <li><a class="footer-link" href="#!">Refund and Returns</a></li>
+          <li><a class="footer-link" href="#!">FAQ</a></li>
+          <li><a class="footer-link" href="#!">Contact us</a></li>
+        </ul>
+      </div>
+      <div class="col l2 s12">
+        <h5 class="white-text">Legal</h5>
+        <ul>
+          <li><a class="footer-link" href="#!">Privacy Policy</a></li>
+          <li><a class="footer-link" href="#!">Copyright Policy</a></li>
+          <li><a class="footer-link" href="#!">Terms of use</a></li>
+        </ul>
+      </div>
+      <div class="col l4 s12">
+        <h5 class="white-text">Contacts</h5>
+        <ul>
+          <li>
+            <a class="footer-link" href="#!"><img src="images/facebook.png" alt="Facebook" width="30" height="30"/></a>&nbsp;
+            <a class="footer-link" href="#!"><img src="images/twitter.png" alt="Twitter" width="30" height="30"/></a>&nbsp;
+            <a class="footer-link" href="#!"><img src="images/google-plus.png" alt="Google+" width="30" height="30"/></a>&nbsp;
+          </li>
+          <li><a class="footer-link" href="#"><i class="mdi-maps-local-phone"></i> 9876543210</a></li>
+        </ul>
       </div>
     </div>
-    <div class="footer-copyright">
-      <div class="container">
-      &copy; Copyright 2015 GetIITians
-      <a class="grey-text text-lighten-4 right" href="#!">www.getiitians.com</a>
-      </div>
+  </div>
+  <div class="footer-copyright">
+    <div class="container">
+    &copy; Copyright 2015 GetIITians
+    <a class="grey-text text-lighten-4 right" href="#!">www.getiitians.com</a>
     </div>
-  </footer>
-
- 
+  </div>
+</footer>
