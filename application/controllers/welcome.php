66f161d31ac1a50be4d2db48a06efe98e7787e1d
Shivam Mamgain <shivammamgain47@gmail.com>
2015-06-29 13:01:20 +0530

    Updated Search page

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -336,7 +336,7 @@ class Welcome extends CI_Controller {
 		$pageinfo["timer"]=Funs::doublesplit($_ginfo['timer']);
 		$pageinfo["search"]=get("q","");
 
-		load_view("search.php",$pageinfo);
+		load_view("search_old.php",$pageinfo);
 	}
 
 
--- application/views/search_old.php
+++ application/views/search_old.php
@@ -7,7 +7,7 @@ load_view("Template/navbarnew.php");
 </script>
 
 <main>
-  <div class="container">0
+  <div class="container">
   <br>
   	<div class="row">
   		<div class="col s12 l4">
@@ -19,7 +19,7 @@ load_view("Template/navbarnew.php");
 							?>
 							<div class="row">
 								<div class="col s12">
-								  <h5 class="teal-text text-darken-1"><i class="mdi-content-filter-list left"></i>Filter</h5>
+								  <h5 class="teal-text text-darken-1"><i class="material-icons left">filter_list</i>Filter</h5>
 								</div>
 							</div>
 							<div class="row">
@@ -33,7 +33,7 @@ load_view("Template/navbarnew.php");
 							</div>
 							<div class="row">
 								<div class="input-field col s12">
-									<select name='subject'  class="browser-default" id='selectsubject' onchange='topicssubtopic_t2(this);' data-condition='simple' >
+									<select name='subject'  class="browser-default" id='selectsubject' onchange='topicssubtopic_t2(this);' data-condition='simple'>
 										<option value="" disabled selected>Select Subject</option>
 									</select>
 								</div>
@@ -43,116 +43,72 @@ load_view("Template/navbarnew.php");
 							    <ul class="collapsible" data-collapsible="expandable">
 										<li>
 										  <div class="collapsible-header">Topics</div>
-											<div class="collapsible-body" id="selecttopic">
-												Select The class
-												<input type='checkbox' style='display:none' name='topic' value='' />
-											 <?php
-												for($i=0;$i<0;$i++){
-											 ?>
-											 <div class="row">
-												<div class="col l12">
-												 <input id="math" type="checkbox" name="sub[]" data-condition="checkbox" data-group="sub" />
-												 <label style="padding-left:1.4em" for="math">
-													Maths
-												 </label>
-												</div>
-											 </div>
-											 <?php
-												}
-											 ?>
+											<div class="collapsible-body" id="selecttopic" style="padding:10px;">
+												<p>Select Class and Subject first.</p>
+												<input type='checkbox' style='display:none' name='topic' value=''/>
 											</div>
 										</li>
 										<li>
 											<div class="collapsible-header">Time</div>
-<!--
-										<div class="collapsible-body">
-										 <?php
-										 foreach($allts as $i=>$val) {
-										 ?>
-										 <div class="row">
-											<?php
-												foreach($val as $j=>$val1){
-											?>
-											<div class="col l6">
-											 <input id="timesearch<?php echo $i."-".$j; ?>" type="checkbox" name="timeslot" checked value="<?php echo $val1[1]; ?>" />
-											 <label style="padding-left:1.4em" for="timesearch<?php echo $i."-".$j; ?>" >
-												<?php echo $val1[0]; ?>
-											 </label>
+											<div class="collapsible-body" style="padding:10px;">
+												<?php
+												  foreach($allts as $i=>$val) {
+														foreach($val as $j=>$val1) {
+												?>
+													  <input id="timesearch<?php echo $i."-".$j; ?>" type="checkbox" name="timeslot" class="filled-in" value="<?php echo $val1[1]; ?>" checked/>
+													  <label for="timesearch<?php echo $i."-".$j; ?>"><?php echo $val1[0]; ?></label><br>
+												<?php
+														}
+												  }
+												?>
 											</div>
-											<?php
-												}
-											?>
-										 </div>
-										 <?php
-										 }
-										 ?>
-										</div>
--->
 										</li>
 										<li>
-											<div class="collapsible-header">Language</div>
-<!--
-										<div class="collapsible-body">
-										 <?php
-											$count=1;
-											foreach($lang as $i=>$val1){
-										 ?>
-										 <div class="row">
-											<?php
-												foreach($val1 as $j=>$val2){
-											?>
-											<div class="col l6">
-											 <input id="lang<?php echo $count; ?>" type="checkbox" name="lang" value='<?php echo $count; ?>' checked />
-											 <label style="padding-left:1.4em" for="lang<?php echo $count; ?>" >
-												<?php echo $val2; ?>
-											 </label>
+											<div class="collapsible-header">Languages</div>
+											<div class="collapsible-body" style="padding:10px;">
+												<?php
+													$count=1;
+													foreach($lang as $i=>$val1) {
+														foreach($val1 as $j=>$val2) {
+												?>
+												  <input id="lang<?php echo $count; ?>" type="checkbox" class="filled-in" name="lang" value='<?php echo $count; ?>' checked />
+												  <label for="lang<?php echo $count; ?>" >
+													  <?php echo $val2; ?>
+												   </label><br>
+												<?php
+														  $count++;
+													  }
+												  }
+												?>
 											</div>
-											<?php
-													$count++;
-												}
-											?>
-										 </div>
-										 <?php
-											}
-										 ?>
-										</div>
 										</li>
--->
 										<li>
 											<div class="collapsible-header">Fees per hour</div>
-<!--
-										<div class="collapsible-body">
-										 <?php
-											$count=1;
-											foreach($price as $val){
-												opent("div",array("class"=>"row"));
-												foreach($val as $val1){
-													load_view("Template/check1.php",array('label'=>htmlspecialchars($val1[0]), 'value'=>$count,"id"=>"price".$count, "name"=>"price"));
-													$count++;
-												}
-												closet("div");
-											}
-										 ?>
-										</div>
--->
+											<div class="collapsible-body" style="padding:10px;">
+												<?php
+													$count=1;
+													foreach($price as $val) {
+														foreach($val as $val1) {
+															load_view("Template/check1.php",array('label'=>htmlspecialchars($val1[0]), 'value'=>$count,"id"=>"price".$count, "name"=>"price"));
+															$count++;
+														}
+													}
+												?>
+											</div>
 										</li>
 										<li>
 											<div class="collapsible-header">Duration</div>
-<!--
-										<div class="collapsible-body">
-										 <?php
-											$count=1;
-											foreach($timer as $val){
-												opent("div",array("class"=>"row"));
-												foreach($val as $val1){
-													load_view("Template/check1.php",array('label'=>htmlspecialchars($val1[0]), 'value'=>$count,"id"=>"timer".$count, "name"=>"timer"));
-													$count++;
-												}
-												closet("div");
-											}
-										 ?>
-										</div>
--->
+											<div class="collapsible-body" style="padding:10px;">
+												<?php
+													$count=1;
+													foreach($timer as $val) {
+														foreach($val as $val1) {
+															load_view("Template/check1.php",array('label'=>htmlspecialchars($val1[0]), 'value'=>$count,"id"=>"timer".$count, "name"=>"timer"));
+															$count++;
+														}
+													}
+												?>
+											</div>
 										</li>
 								  </ul>
 							  </div>
@@ -165,7 +121,7 @@ load_view("Template/navbarnew.php");
   			<div class="card-panel">
   			  <div class="row">
 						<div class="col s12">
-						  <h5 class="teal-text text-darken-1"><i class="mdi-action-search left"></i>Search</h5>
+						  <h5 class="teal-text text-darken-1"><i class="material-icons left">search</i>Search</h5>
 						</div>
 					</div>
   				<div class="row">
@@ -187,7 +143,7 @@ load_view("Template/navbarnew.php");
 						  </div>
 						</form>
   				</div>
-  				<div class="divider"></div>
+  				<div class="divider"></div><br>
   				<div class="row">
   					<div class="col s12">
   						<div id="searchresultdiv" data-action='search' data-max='<?php echo $_ginfo["numsearchr"]["loadonce"]; ?>' data-maxl='<?php echo $_ginfo["numsearchr"]["loadadd"]; ?>' data-eparams='searchform()' data-ignoreloadonce='<?php echo $_ginfo["numsearchr"]["loadonce"]; ?>'>
