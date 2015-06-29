cc017ad31ed772cbf052472b01d14153ba769899
Shivam Mamgain <shivammamgain47@gmail.com>
2015-06-29 13:09:36 +0530

    New Materialize files

--- application/views/Template/navbarnew.php
+++ application/views/Template/navbarnew.php
@@ -10,9 +10,9 @@
     <nav class="teal darken-4" role="navigation">
       <div class="nav-wrapper">
         <a href="#" class="brand-logo" style="padding-left:20px;">getIITians</a>
-        <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
+        <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><a href="<?php echo BASE."aboutus"; ?>">About Us</a></li>
+          <li><a href="<?php echo BASE."aboutus"; ?>">About</a></li>
           <li><a href="<?php echo BASE."contactus"; ?>">Contact Us</a></li>
 
           <?php
@@ -27,7 +27,7 @@
           <li><a href="<?php echo BASE."login"; ?>">Login</a></li>
           <li>
             <a class="dropdown-button" href="#" data-beloworigin="true" data-activates="dropdownaccount">
-              Create Account <i class="mdi-navigation-arrow-drop-down right"></i>
+              Create Account <i class="material-icons right">arrow_drop_down</i>
             </a>
           </li>
           <?php
@@ -37,7 +37,7 @@
           <form class="right">
             <div class="input-field">
               <input id="search" type="search" placeholder="Search Tutors" autocomplete="off">
-              <label for="search"><i class="mdi-action-search"></i></label>
+              <label for="search"><i class="material-icons">search</i></label>
             </div>
           </form>
         </ul>
@@ -47,7 +47,7 @@
 
   <!-- SideNav -->
   <ul class="side-nav" id="mobile-demo">
-    <li><a href="<?php echo BASE."aboutus"; ?>">About Us</a></li>
+    <li><a href="<?php echo BASE."aboutus"; ?>">About</a></li>
     <li><a href="<?php echo BASE."contactus"; ?>">Contact Us</a></li>
 
     <?php
@@ -76,7 +76,7 @@
       <form class="col s12">
         <div class="row">
           <div class="input-field col s12">
-            <i class="mdi-action-search prefix"></i>
+            <i class="material-icons prefix">search</i>
             <input id="sidenav_search" type="text">
             <label for="sidenav_search">Search Tutors</label>
           </div>
