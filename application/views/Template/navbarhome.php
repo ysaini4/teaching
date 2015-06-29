f4bcd4d35211ab5e71a55818ddb7b6812bea1712
mohit <mohit@mohit>
2015-06-29 10:13:30 +0530

    Modf home page

--- application/views/Template/navbarhome.php
+++ application/views/Template/navbarhome.php
@@ -1,52 +1,86 @@
 <body>
+  <!-- Dropdown for Create Account -->
   <ul id="dropdownaccount" class="dropdown-content">
-      <?php 
-      if (User::islogin()){ ?>
-      <li><a href="<?php echo BASE."profile" ;?>">Profile</a></li>
-      <li><a href="<?php echo BASE."account" ;?>">Account</a></li>
-      <li><a href="<?php echo BASE."?logout" ;?>">Logout</a></li>
-       <?php 
-     }
-     else{
-      ?>
-      <li><a href="<?php echo BASE."login" ;?>">Login</a></li>
-      <li><a href="<?php echo BASE."signup" ;?>">Signup</a></li>
-      <li><a href="<?php echo BASE."joinus" ;?>">Join Us</a></li>
-      <?php
-     }
-
-      ?>
+    <li><a href="<?php echo BASE."signup" ;?>">Signup&nbsp;&nbsp;&nbsp;&nbsp;<span style="font-size:13px;" class="grey-text">for Students</span></a></li>
+    <li><a href="<?php echo BASE."joinus" ;?>">Joinus&nbsp;&nbsp;&nbsp;&nbsp;<span style="font-size:13px;" class="grey-text">for Tutors</span></a></li>
   </ul>
+
   <!-- NavBar -->
-  <div class="navbar-fixed">
-    <nav class="transparent" id="navbar" role="navigation">
+  <div class="navbar">
+    <nav class="transparent" role="navigation" id="navbar">
       <div class="nav-wrapper">
         <a href="#" class="brand-logo"><img src="images/logo.png" width="190" height="81" style="padding-top:10px;"/></a>
         <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><a href="<?php echo BASE."aboutus" ;?>">About</a></li>
-          <li><a href="<?php echo BASE."contactus" ;?>">Contact us</a></li>
-          <li><a class="dropdown-button" href="#" data-beloworigin="true" data-activates="dropdownaccount"><i class="mdi-action-account-circle left"></i>Account</a></li>
+          <li><a href="<?php echo BASE."aboutus"; ?>">About Us</a></li>
+          <li><a href="<?php echo BASE."contactus"; ?>">Contact Us</a></li>
+
+          <?php
+          if (User::islogin()) {
+          ?>
+          <li><a href="<?php echo BASE."profile" ;?>">Profile</a></li>
+          <li><a href="<?php echo BASE."?logout"; ?>">Logout</a></li>
+          <?php
+          }
+          else {
+          ?>
+          <li><a href="<?php echo BASE."login"; ?>">Login</a></li>
+          <li>
+            <a class="dropdown-button" href="#" data-beloworigin="true" data-activates="dropdownaccount">
+              Create Account <i class="mdi-navigation-arrow-drop-down right"></i>
+            </a>
+          </li>
+          <?php
+          }
+          ?>
+
           <form class="right">
-            <div class="input-field transparent lighten-1">
-              <input id="search" type="search" placeholder="Search" required>
+            <div class="input-field">
+              <input id="search" type="search" placeholder="Search Tutors" autocomplete="off">
               <label for="search"><i class="mdi-action-search"></i></label>
-              <i class="mdi-navigation-close"></i>
             </div>
           </form>
         </ul>
       </div>
     </nav>
   </div>
+
   <!-- SideNav -->
   <ul class="side-nav" id="mobile-demo">
-    <li><a href="<?php echo BASE."aboutus" ;?>" >About</a></li>
-    <li><a href="<?php echo BASE."contactus" ;?>" >Contact</a></li>
-    <form>
-      <div class="input-field">
-        <input id="search" type="search" required>
-        <label for="search"><i class="mdi-action-search"></i></label>
-        <i class="mdi-navigation-close"></i>
-      </div>
-    </form>
+    <li><a href="<?php echo BASE."aboutus"; ?>">About Us</a></li>
+    <li><a href="<?php echo BASE."contactus"; ?>">Contact Us</a></li>
+
+    <?php
+    if (User::islogin()) {
+    ?>
+    <li><a href="<?php echo BASE."profile" ;?>">Profile</a></li>
+    <li><a href="<?php echo BASE."?logout"; ?>">Logout</a></li>
+    <?php
+    }
+    else {
+    ?>
+    <li><a href="<?php echo BASE."login"; ?>">Login</a></li>
+    <li>
+      <a href="<?php echo BASE."signup" ;?>">Signup&nbsp;&nbsp;&nbsp;&nbsp;
+      <span style="font-size:13px;" class="grey-text">for Students</span></a>
+    </li>
+    <li>
+      <a href="<?php echo BASE."joinus" ;?>">Joinus&nbsp;&nbsp;&nbsp;&nbsp;
+      <span style="font-size:13px;" class="grey-text">for Tutors</span></a>
+    </li>
+    <?php
+    }
+    ?>
+
+    <div class="row">
+      <form class="col s12">
+        <div class="row">
+          <div class="input-field col s12">
+            <i class="mdi-action-search prefix"></i>
+            <input id="sidenav_search" type="text">
+            <label for="sidenav_search">Search Tutors</label>
+          </div>
+        </div>
+      </form>
+    </div>
   </ul>
