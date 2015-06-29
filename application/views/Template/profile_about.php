b9313605f351b0c8e061a589a5647f3c33b10a26
Shivam Mamgain <shivammamgain47@gmail.com>
2015-06-29 10:40:30 +0530

    Updated Teacher Profile Page

---
+++ application/views/Template/profile_about.php
@@ -0,0 +1,169 @@
+<br><br>
+<div class="row">
+  <div class="col s12">
+    <h5 class="teal-text text-darken-1">Profile Details</h5>
+  </div>
+</div>
+<div id="profile_info">
+
+  <!-- Basic Details -->
+  <div class="row">
+    <div class="col s12">
+      <h6 class="teal-text text-darken-1"><i class="material-icons left">perm_identity</i>Basic</h6>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col s12 l6">
+      <div class="row">
+        <div class="col s12">
+          Name :
+          <span class="grey-text text-darken-1">
+            <?php echo Fun::smilymsg($firstName) ." ". Fun::smilymsg($lastName); ?>
+          </span>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col s12">
+          Email :
+          <span class="grey-text text-darken-1">
+            <?php echo $aboutinfo['email']; ?>
+          </span>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col s12">
+          Birthday :
+          <span class="grey-text text-darken-1">
+            <?php echo date('d-m-Y',$aboutinfo['dob']); ?>
+          </span>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col s12">
+          Gender :
+          <span class="grey-text text-darken-1">
+          <?php
+          if (Fun::smilymsg($aboutinfo['gender']) == 'm')
+            echo 'Male';
+          else if (Fun::smilymsg($aboutinfo['gender']) == 'f')
+            echo 'Female';
+          else
+            echo 'Other';
+          ?>
+          </span>
+        </div>
+      </div>
+    </div>
+    <div class="col s12 l6">
+      <div class="row">
+        <div class="col s12">
+          Address :<br>
+          <span class="grey-text text-darken-1">
+            <?php echo Fun::smilymsg($jsonArray['city']) ."<br>". Fun::smilymsg($jsonArray['state']) .", ". Fun::smilymsg($jsonArray['zipcode']) ."<br>". Fun::smilymsg($jsonArray['country']); ?>
+          </span>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col s12">
+          Mobile Number :
+          <span class="grey-text text-darken-1">
+            <?php echo Fun::smilymsg($aboutinfo['phone']); ?>
+          </span>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col s12">
+          Resume :
+          <span class="grey-text text-darken-1">
+            <a href="#!">Click to see</a>
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Teaching Details -->
+  <div class="row">
+    <div class="col s12">
+      <h6 class="teal-text text-darken-1"><i class="material-icons left">subject</i>Teaching Details</h6>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col s12 l3">
+      Subjects :
+      <span class="grey-text text-darken-1">
+        <ul>
+          <?php
+            foreach ($subArray as $value) {
+              echo '<li>'.$value.'</li>';
+            }
+          ?>
+        </ul>
+      </span>
+    </div>
+    <div class="col s12 l2">
+      Grades :
+      <span class="grey-text text-darken-1">
+        <ul>
+          <?php
+            foreach ($gradeArray as $value) {
+              echo '<li>'.$value.'</li>';
+            }
+         ?>
+        </ul>
+      </span>
+    </div>
+    <div class="col s12 l4">
+      Languages :
+      <span class="grey-text text-darken-1">
+        <ul>
+          <?php
+            foreach ($langArray as $value) {
+              echo '<li>'.$value.'</li>';
+            }
+         ?>
+        </ul>
+      </span>
+    </div>
+  </div>
+
+  <!-- Education and Qualification Details -->
+  <div class="row">
+    <div class="col s12">
+      <h6 class="teal-text text-darken-1"><i class="material-icons left">school</i>Education and Qualifications</h6>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col s12">
+      <div class="row">
+        <div class="col s12">
+          College :
+          <span class="grey-text text-darken-1">
+            <?php echo 'IIT '. $jsonArray['college']; ?>
+          </span>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col s12">
+          Degree :
+          <span class="grey-text text-darken-1">
+            <?php
+            echo Fun::smilymsg($jsonArray['degree']);
+            if ($jsonArray['degreeother'] != '')
+              echo ' , '.Fun::smilymsg($jsonArray['degreeother']);
+            ?>
+          </span>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col s12">
+          Branch :
+          <span class="grey-text text-darken-1">
+            <?php echo Fun::smilymsg($jsonArray['branch']); ?>
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
---
+++ application/views/Template/profile_reviews.php
@@ -0,0 +1,70 @@
+<br><br>
+<div class="row">
+  <div class="col s12 l10 offset-l1">
+    <h5 class="teal-text text-darken-1">Student reviews about you</h5>
+  </div>
+</div>
+
+<!-- Review Cards  -->
+<div class="row">
+  <div class="col s12 l10 offset-l1">
+    <div class="card white">
+      <div class="card-content grey-text">
+        <h6>
+          <a href="#"><i class="material-icons small left">face</i>Prof. Severus Snape</a>
+        </h6><br>
+        <p>He is an excellent tutor. Explained concepts better than school teachers did.</p>
+      </div>
+      <div class="card-action">
+        <a href="#">Upvote</a>
+        <a href="#">Downvote</a>
+        <a class="modal-trigger" href="#upvote_modal" onclick="smgPreloader();">15&nbsp;<i class="material-icons tiny">thumb_up</i></a>
+        <a class="modal-trigger" href="#downvote_modal" onclick="smgPreloader();">2&nbsp;<i class="material-icons tiny">thumb_down</i></a>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col s12 l10 offset-l1">
+    <div class="card white">
+      <div class="card-content grey-text">
+        <h6>
+          <a href="#"><i class="material-icons small left">face</i>Dr. Shivam Mamgain</a>
+        </h6><br>
+        <p>A terrible and arrogant teacher. He has no respect for the students and thinks students and donkeys are alike.</p>
+      </div>
+      <div class="card-action">
+        <a href="#">Upvote</a>
+        <a href="#">Downvote</a>
+        <a class="modal-trigger" href="#upvote_modal" onclick="smgPreloader();">39&nbsp;<i class="material-icons tiny">thumb_up</i></a>
+        <a class="modal-trigger" href="#downvote_modal" onclick="smgPreloader();">11&nbsp;<i class="material-icons tiny">thumb_down</i></a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+  <!-- Upvote Modal Structure -->
+  <div id="upvote_modal" class="modal">
+    <div class="modal-content">
+      <h5 class="green-text"><i class="material-icons left">thumb_up</i>Upvotes</h5>
+      <div class="smg-preloader"></div>
+      <div class="smg">
+        <p><a href="#"><i class="material-icons left">tag_faces</i>Babloo</a></p>
+        <p><a href="#"><i class="material-icons left">tag_faces</i>Pickachu</a></p>
+        <p><a href="#"><i class="material-icons left">tag_faces</i>Partap</a></p>
+      </div>
+    </div>
+  </div>
+  <!-- Downvote Modal Structure -->
+  <div id="downvote_modal" class="modal">
+    <div class="modal-content">
+      <h5 class="green-text"><i class="material-icons left">thumb_down</i>Downvotes</h5>
+      <div class="smg-preloader"></div>
+      <div class="smg">
+        <p><a href="#"><i class="material-icons left">tag_faces</i>Babloo</a></p>
+        <p><a href="#"><i class="material-icons left">tag_faces</i>Pickachu</a></p>
+        <p><a href="#"><i class="material-icons left">tag_faces</i>Partap</a></p>
+      </div>
+    </div>
+  </div>
