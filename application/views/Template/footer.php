bc1cb65d78086224716eff458f283d1d7fc37fad
Shivam Mamgain <shivammamgain47@gmail.com>
2015-06-30 19:01:08 +0530

    Reverted to older theme

--- application/views/Template/footer.php
+++ application/views/Template/footer.php
@@ -1,16 +1,19 @@
 <!-- Footer -->
-<footer class="page-footer teal">
+<footer class="page-footer teal darken-3">
   <div class="container">
     <div class="row">
       <div class="col l4 s12">
-        <h5 class="white-text">Get IITians</h5>
-        <p class="white-text"></p>
+        <h5 class="white-text" style="font-family: Kurale;">GetIITians</h5>
+        <p class="white-text"><i class="material-icons tiny">mail</i> mail@getiitians.com</p>
+        <?php /*
         <ul>
           <li><a class="footer-link" href="<?php echo BASE.'aboutus'; ?>">About Us</a></li>
           <li><a class="footer-link" href="#!">Our Team</a></li>
           <li><a class="footer-link" href="#!">Work with us</a></li>
         </ul>
+        */ ?>
       </div>
+      <?php /*
       <div class="col l2 s12">
         <h5 class="white-text">Help</h5>
         <ul>
@@ -27,20 +30,31 @@
           <li><a class="footer-link" href="#!">Terms of use</a></li>
         </ul>
       </div>
-      <div class="col l4 s12">
-        <h5 class="white-text">Contacts</h5>
+      <div class="col l3 s12">
+        <h5 class="white-text">Contact Us</h5>
         <ul>
+          <li><a class="footer-link"><i class="material-icons tiny">phone</i> 9876543210</a></li>
+          <li><a class="footer-link"><i class="material-icons tiny">mail</i> mail [at] getiitians.com</a></li>
           <li>
-            <a class="footer-link" href="#!"><img src="images/facebook.png" alt="Facebook" width="30" height="30"/></a>&nbsp;
-            <a class="footer-link" href="#!"><img src="images/twitter.png" alt="Twitter" width="30" height="30"/></a>&nbsp;
-            <a class="footer-link" href="#!"><img src="images/google-plus.png" alt="Google+" width="30" height="30"/></a>&nbsp;
+            <a class="footer-link tooltipped" href="#!" data-position="bottom" data-delay="50" data-tooltip="Facebook">
+              <img src="images/facebook-ribbon.png" alt="Facebook" width="30" height="60"/>
+            </a>
+            <a class="footer-link tooltipped" href="#!" data-position="bottom" data-delay="50" data-tooltip="Twitter">
+              <img src="images/twitter-ribbon.png" alt="Twitter" width="30" height="60"/>
+            </a>
+            <a class="footer-link tooltipped" href="#!" data-position="bottom" data-delay="50" data-tooltip="Google+">
+              <img src="images/googleplus-ribbon.png" alt="Google+" width="30" height="60"/>
+            </a>
+            <a class="footer-link tooltipped" href="mailto:mail@getiitians.com" data-position="bottom" data-delay="50" data-tooltip="mail@getiitians.com">
+              <img src="images/email-ribbon.png" alt="Email" width="30" height="60"/>
+            </a>
           </li>
-          <li><a class="footer-link" href="#"><i class="mdi-maps-local-phone"></i> 9876543210</a></li>
         </ul>
       </div>
+      */ ?>
     </div>
   </div>
-  <div class="footer-copyright teal darken-1">
+  <div class="footer-copyright teal darken-2">
     <div class="container">
     &copy; Copyright 2015 GetIITians
     <a class="grey-text text-lighten-4 right" href="#!">www.getiitians.com</a>
--- application/views/Template/navbarnew.php
+++ application/views/Template/navbarnew.php
@@ -1,30 +1,30 @@
 <body>
   <!-- Dropdown for Create Account -->
   <ul id="dropdownaccount" class="dropdown-content">
-    <li><a href="<?php echo BASE."signup" ;?>">Signup&nbsp;&nbsp;&nbsp;&nbsp;<span style="font-size:13px;" class="grey-text">for Students</span></a></li>
-    <li><a href="<?php echo BASE."joinus" ;?>">Joinus&nbsp;&nbsp;&nbsp;&nbsp;<span style="font-size:13px;" class="grey-text">for Tutors</span></a></li>
+    <li class="<?php pit('active', $page==='signup');?>"><a href="<?php echo BASE."signup" ;?>">Signup&nbsp;&nbsp;&nbsp;&nbsp;<span style="font-size:13px;" class="grey-text">for Students</span></a></li>
+    <li class="<?php pit('active', $page==='joinus');?>"><a href="<?php echo BASE."joinus" ;?>">Joinus&nbsp;&nbsp;&nbsp;&nbsp;<span style="font-size:13px;" class="grey-text">for Tutors</span></a></li>
   </ul>
 
   <!-- NavBar -->
   <div class="navbar-fixed">
-    <nav class="teal" role="navigation">
+    <nav class="teal darken-3" role="navigation">
       <div class="nav-wrapper">
         <a href="#" class="brand-logo">getIITians</a>
         <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><a href="<?php echo BASE."aboutus"; ?>">About</a></li>
-          <li><a href="<?php echo BASE."contactus"; ?>">Contact Us</a></li>
+          <li class="<?php pit('active', $page==='aboutus');?>"><a href="<?php echo BASE."aboutus"; ?>">About</a></li>
+          <li class="<?php pit('active', $page==='contactus');?>"><a href="<?php echo BASE."contactus"; ?>">Contact Us</a></li>
 
           <?php
           if (User::islogin()) {
           ?>
-          <li><a href="<?php echo BASE."profile" ;?>">Profile</a></li>
+          <li class="<?php pit('active', $page==='profile');?>"><a href="<?php echo BASE."profile" ;?>">Profile</a></li>
           <li><a href="<?php echo BASE."?logout"; ?>">Logout</a></li>
           <?php
           }
           else {
           ?>
-          <li><a href="<?php echo BASE."login"; ?>">Login</a></li>
+          <li class="<?php pit('active', $page==='login');?>"><a href="<?php echo BASE."login"; ?>">Login</a></li>
           <li>
             <a class="dropdown-button" href="#" data-beloworigin="true" data-activates="dropdownaccount">
               Create Account <i class="material-icons right">arrow_drop_down</i>
@@ -35,7 +35,7 @@
           ?>
 
           <form class="right" action="<?php echo BASE."search"; ?>" >
-            <div class="input-field teal darken-1">
+            <div class="input-field teal darken-4">
               <input id="search" type="search" placeholder="Search Tutors" autocomplete="off" name="q" >
               <label for="search"><i class="material-icons">search</i></label>
               <button type="submit" style="display:none;" ></button>
@@ -48,24 +48,24 @@
 
   <!-- SideNav -->
   <ul class="side-nav" id="mobile-demo">
-    <li><a href="<?php echo BASE."aboutus"; ?>">About</a></li>
-    <li><a href="<?php echo BASE."contactus"; ?>">Contact Us</a></li>
+    <li class="<?php pit('active', $page==='aboutus');?>"><a href="<?php echo BASE."aboutus"; ?>">About</a></li>
+    <li class="<?php pit('active', $page==='contactus');?>"><a href="<?php echo BASE."contactus"; ?>">Contact Us</a></li>
 
     <?php
     if (User::islogin()) {
     ?>
-    <li><a href="<?php echo BASE."profile" ;?>">Profile</a></li>
+    <li class="<?php pit('active', $page==='profile');?>"><a href="<?php echo BASE."profile" ;?>">Profile</a></li>
     <li><a href="<?php echo BASE."?logout"; ?>">Logout</a></li>
     <?php
     }
     else {
     ?>
-    <li><a href="<?php echo BASE."login"; ?>">Login</a></li>
-    <li>
+    <li class="<?php pit('active', $page==='login');?>"><a href="<?php echo BASE."login"; ?>">Login</a></li>
+    <li class="<?php pit('active', $page==='signup');?>">
       <a href="<?php echo BASE."signup" ;?>">Signup&nbsp;&nbsp;&nbsp;&nbsp;
       <span style="font-size:13px;" class="grey-text">for Students</span></a>
     </li>
-    <li>
+    <li class="<?php pit('active', $page==='joinus');?>">
       <a href="<?php echo BASE."joinus" ;?>">Joinus&nbsp;&nbsp;&nbsp;&nbsp;
       <span style="font-size:13px;" class="grey-text">for Tutors</span></a>
     </li>
@@ -73,14 +73,19 @@
     }
     ?>
 
-    <div class="row">
+    <div class="row grey lighten-3">
       <form class="col s12">
         <div class="row">
           <div class="input-field col s12">
-            <i class="material-icons prefix">search</i>
-            <input id="sidenav_search" type="text" name="q">
+            <input id="sidenav_search" type="text" name="q" autocomplete="off">
             <label for="sidenav_search">Search Tutors</label>
-            <button type="submit" style="display:none;" ></button>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col s12">
+            <button type="submit" class="btn waves-effect waves-light">Search
+              <i class="material-icons right">search</i>
+            </button>
           </div>
         </div>
       </form>
