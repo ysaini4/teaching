9630dba645c0e9c4d5d725a6d5d0ee101f61558e
Shivam Mamgain <shivammamgain47@gmail.com>
2015-07-01 14:30:32 +0530

    Fixed filter panel in Search

---
+++ js/search.js
@@ -0,0 +1,18 @@
+/* Search Page - Filter Panel */
+var sticker = $("#sticker_panel");
+var pos_stk = sticker.position(); 
+
+$(window).scroll(function() {
+  var windowpos = $(window).scrollTop() + 60;
+  var stickermax = $(document).outerHeight() - $("#footer").outerHeight() - sticker.outerHeight() - 60;
+
+  if (windowpos >= pos_stk.top && windowpos < stickermax) {
+    sticker.attr("style", ""); // kill absolute positioning
+    sticker.addClass("stick"); // stick it
+  } else if (windowpos >= stickermax) {
+    sticker.removeClass();     // un-stick
+    sticker.css({position: "absolute", top: stickermax + "px"}); //set sticker right above the footer
+  } else {
+    sticker.removeClass(); //top of page
+  }
+});
