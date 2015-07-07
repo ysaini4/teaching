f61c42e3e6342f724b8ba42c4fe1a11b558876eb
Shivam Mamgain <shivammamgain47@gmail.com>
2015-07-07 12:55:35 +0530

    Profile pic relocated

--- application/views/studentprofile.php
+++ application/views/studentprofile.php
@@ -6,62 +6,6 @@ load_view("Template/navbarnew.php",$inp);
     <div class="container">
     <br>
       <div class="card-panel">
-      <br>
-        <div class="row">
-          <div class="col s12 l4 offset-l1 center">
-            <img class="materialboxed" height="100" width="100" src="<?php  echo $sinfo["profilepic"]; ?>">
-            <br>
-            <!-- Change Profile Picture -->
-            <?php
-              if(User::loginId() == $sid){
-            ?>
-           <form method="post" enctype="multipart/form-data"> 
-            <a onclick='uploadfile(this,"profilepic");' style="cursor:pointer;" >Change Profile Picture</a>
-           </form>
-           <?php
-            }
-           ?>
-
-
-            <div id="pic_upload" class="modal">
-              <div class="modal-content">
-                <h6 class="teal-text">Change Profile Picture</h6>
-              </div>
-
-              <div class="row">
-                <form action="#" class="col s12 l8 offset-l2">
-                  <div class="row">
-                    <div class="file-field input-field col s12">
-                      <input class="file-path validate" type="text">
-                      <div class="btn waves-effect waves-light blue">
-                        <span>Open</span>
-                        <input type="file">
-                      </div>
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col s12">
-                      <button type="submit" class="btn waves-effect waves-light white grey-text">Change</button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </div>
-            <!-- End -->
-          </div>
-          <div class="col s12 l7">
-            <div class="row">
-              <div class="col s12">
-                <h5 class="green-text left"><?php echo $sinfo["name"]; ?></h5>
-              </div>
-              <div class="col s12">
-                <h6 class="grey-text left"><?php echo $sinfo["email"]; ?></h6>                
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="card-panel">
         <div class="row">
           <div class="col s12">
             <ul class="tabs">
