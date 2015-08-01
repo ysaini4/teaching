ace548c9239c101c41292a9e9aeb67b955a28cc3
ysaini4 <ysaini75@gmail.com>
2015-08-01 15:50:22 +0530

    changes

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -205,9 +205,9 @@ class Welcome extends CI_Controller {
 	}
 	
 		public function profile($tid=0,$tabid=1) { 
-			$this->load->library('uri'); 
-			$tid = $this->uri->segment(2);
-			$tabid = $this->uri->segment(3);
+			//$this->load->library('uri'); 
+			//$tid = $this->uri->segment(2);
+			//$tabid = $this->uri->segment(3);
 			$numtabs=5;
 			global $_ginfo;
 			$tid=Funs::gettid($tid);
